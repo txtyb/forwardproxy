6c9b4db7c10a39f87b4aeed2b40cfcd166909938
klzgrad <kizdiv@gmail.com>
2022-10-06 18:44:47 +0800

    Fix rand seeding

--- forwardproxy.go
+++ forwardproxy.go
@@ -48,6 +48,10 @@ import (
 
 func init() {
 	caddy.RegisterModule(Handler{})
+
+	// Used for generating padding lengths. Not needed to be cryptographically secure.
+	// Does not care about double seeding.
+	rand.Seed(time.Now().UnixNano())
 }
 
 // Handler implements a forward proxy.
